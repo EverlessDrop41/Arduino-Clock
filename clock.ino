7ee80525350083399c57e3b34dda75eedb6ae3ba
Emily Peregrine <tomfperegrine@googlemail.com>
2015-09-23 22:52:57 +0100

    Added days and fixed hour timing

--- clock.ino
+++ clock.ino
@@ -5,41 +5,67 @@ const int TIMESTEP = 1000; //delay between each update
 const byte SEC_IN_MIN = 60;
 const byte MIN_IN_HOUR = 60;
 const byte HOUR_IN_DAY = 24;
+const byte DAYS_IN_WEEK = 7; //Maximum Day Index + 1
 
-byte currentSecs = 0;
-byte currentMins = 0;
-byte currentHours = 0;
+const char dayToString[DAYS_IN_WEEK][10] = {
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+  "Sunday"
+};
+
+byte currentSecs = 55;
+byte currentMins = 59;
+byte currentHours = 23;
+byte currentDayIndex = 5;
 
 void UpdateTime () {
-	currentSecs++;
+  currentSecs++;
 
-	if (currentSecs >= SEC_IN_MIN) {
-		currentSecs = 0;
-		currentMins++;
-	}
+  if (currentSecs >= SEC_IN_MIN) {
+    currentSecs = 0;
+    currentMins++;
+  }
 
-	if (currentMins >= MIN_IN_HOUR) {
-		currentMins = 0;
-		currentHours++;
-	}
+  if (currentMins >= MIN_IN_HOUR) {
+    currentMins = 0;
+    currentHours++;
+  }
+
+  if (currentHours >= HOUR_IN_DAY) {
+    currentHours = 0;
+    currentDayIndex++;
+  }
+
+  if (currentDayIndex >= DAYS_IN_WEEK) {
+    currentDayIndex = 0;
+  }
 }
 
 void lcdClear() {
-	lcd.clear();
-	lcd.setCursor(0,0);
+  lcd.clear();
+  lcd.setCursor(0,0);
 }
 
 void setup()
 {
-	lcd.begin(16, 2);
-	lcd.print("Setup Func");
+  lcd.begin(16, 2);
+  lcd.print("Setup Func");
 }
 
 void loop()
 {
-	UpdateTime();
-	lcdClear();
-	String s = String(currentHours) + String(':') + String(currentMins) + String(':') + String(currentSecs);
-	lcd.print(s);
+  UpdateTime();
+  //Update Diplay
+  lcdClear();
+  lcd.print(dayToString[currentDayIndex]);
+  lcd.setCursor(0,1);
+  String s = String(currentHours) + String(':') + String(currentMins) + String(':') + String(currentSecs);
+  lcd.print(s);
+  //Delay for timing
   delay(TIMESTEP);
 }
+
